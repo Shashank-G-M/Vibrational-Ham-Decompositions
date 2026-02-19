86ffe32949cdf32f3cf9099402381c8e9df298aa
Shashank-G-M <mehendalesha@gmail.com>
2026-02-19 13:03:33 -0500

    Load fragments from alternate paths

--- vib_frags/io_utils.py
+++ vib_frags/io_utils.py
@@ -41,6 +41,50 @@ def load_23_mode_params(mol, nmodes, nmodals, loc=True):
     
     return params[:nf], norms[:nf+1], H2_frag, H3_frag
 
+
+
+
+
+
+#Load in fragments that were found in earlier calculation
+def load_23_mode_params_new(mol, nmodes, nmodals, loc=True):
+    '''
+    Parameters
+    ----------
+    params : array of fragment parameters
+    norms : array of norm of two-body tensors after removing fragments
+
+    Reads params and norms from file   
+    Return fragments such that residual norm <= 0.05
+    '''
+    if loc == True:
+      with open(rf"VSCF_basis_loc_frags/two_and_three_mode/{mol}_4T3M_VSCFfragments_loc{str(nmodals)}modals.out", 'rb') as f:
+          params, norms = pickle.load(f)
+    else:
+      with open(rf"VSCF_basis_frags/{mol}_VSCFfragments_{str(nmodals)}M_3MC.out", 'rb') as f:
+          params, norms = pickle.load(f)
+
+ 
+    nf = len(params)
+    H2_frag = np.zeros((nf,nmodes,nmodes,nmodals,nmodals,nmodals,nmodals))
+    H3_frag = np.zeros((nf,nmodes,nmodes,nmodes,nmodals,nmodals,nmodals,nmodals,nmodals,nmodals))
+    for i in range(nf):
+        H2_frag[i], H3_frag[i] = get_fragment_23(params[i], nmodals, nmodes)
+        # if np.sqrt(norms[i+1]/norms[0]) >= 0.001:
+        #     H2_frag[i], H3_frag[i] = get_fragment_23(params[i], nmodals, nmodes)
+        # else:
+        #     H2_frag[i], H3_frag[i] = get_fragment_23(params[i], nmodals, nmodes)
+        #     print(f'Total number of fragments included = {i+1}')
+        #     break
+    # nf = i+1  #Number of fragments included
+    # H2_frag = H2_frag[:nf] 
+    # H3_frag = H3_frag[:nf]
+
+    
+    return params[:nf], norms[:nf+1], H2_frag, H3_frag
+
+
+
 #_______________________________________________________________________________For 2 mode frag___________________________________________________________________________
 
 #Load in fragments that were found in earlier calculation
@@ -59,6 +103,43 @@ def load_2_mode_params(mol, nmodes, nmodals, loc=True):
           params, norms = pickle.load(f)
     else:
       with open(rf"VSCF_basis_frags/only_two_mode/{mol}_VSCFfragments_{str(nmodals)}modals.out", 'rb') as f:
+          params, norms = pickle.load(f)
+ 
+    nf = len(params)
+    H2_frag = np.zeros((nf,nmodes,nmodes,nmodals,nmodals,nmodals,nmodals))
+    for i in range(nf):
+       H2_frag[i] = get_fragment_2(params[i], nmodals, nmodes)
+    #   if np.sqrt(norms[i+1]/norms[0]) >= 0.05:
+    #      H2_frag[i] = get_fragment_2(params[i], nmodals, nmodes)
+    #   else:
+    #      H2_frag[i] = get_fragment_2(params[i], nmodals, nmodes)
+    #      print(f'Total number of fragments included = {i+1}')
+    #      break
+    # nf = i+1  #Number of fragments included
+    # H2_frag = H2_frag[:nf]
+    
+    return params[:nf], norms[:nf+1], H2_frag
+
+
+
+
+
+#Load in fragments that were found in earlier calculation
+def load_2_mode_params_new(mol, nmodes, nmodals, loc=True):
+    '''
+    Parameters
+    ----------
+    params : array of fragment parameters
+    norms : array of norm of two-body tensors after removing fragments
+
+    Reads params and norms from file   
+    Return fragments such that residual norm <= 0.05
+    '''
+    if loc == True:
+      with open(rf"VSCF_basis_loc_frags/only_two_mode/{mol}_VSCFfragments_loc{str(nmodals)}modals.out", 'rb') as f:
+          params, norms = pickle.load(f)
+    else:
+      with open(rf"VSCF_basis_frags/{mol}_VSCFfragments_{str(nmodals)}M_2MC.out", 'rb') as f:
           params, norms = pickle.load(f)
  
     nf = len(params)
